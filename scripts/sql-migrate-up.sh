d5c4309f84473f1e6c6c5e95cdf32c6d9ff1700b
sky0621 <sky14work@gmail.com>
2020-08-11 21:27:33 +0900

    ref

--- scripts/sql-migrate-up.sh
+++ scripts/sql-migrate-up.sh
@@ -4,6 +4,13 @@ SCRIPT_DIR=$(dirname $0)
 echo ${SCRIPT_DIR}
 cd ${SCRIPT_DIR} && cd ../db
 
+env=${1:-}
+
+if [ -z "${env}" ]; then
+  echo -n "Select env(local/localtest/dev): "
+  read env
+fi
+
 # https://github.com/rubenv/sql-migrate
 #go get -v github.com/rubenv/sql-migrate/...
-sql-migrate up -env="local"
+sql-migrate up -env="${env}"
